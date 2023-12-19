a1deeba90225ab274da9fba8e91ee67d6b574d2d
benisprlh <benisaprulah5@gmail.com>
2023-12-19 21:49:58 +0700

    add product seller

--- src/components/seller-components/AddProduct.jsx
+++ src/components/seller-components/AddProduct.jsx
@@ -1,4 +1,39 @@
+import { useEffect, useState } from 'react';
+import api from '../../api';
+import { useSearchParams } from 'react-router-dom';
+
 export default function AddProduct() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [products, setProducts] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(false);
+  const [productAdded, setProductAdded] = useState(0);
+
+  const fetch = async () => {
+    try {
+      const { data } = await api.get('/products');
+      setProducts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const addProduct = async () => {
+    try {
+      const { data } = api.post('/seller-products', {
+        Headers: {
+          Authorization: `Bearer `,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetch();
+    console.log(selectedProduct);
+  }, [selectedProduct]);
+
   return (
     <section id="add-product" className="p-8">
       <h1 className="text-xl font-bold mb-2">Tambah Produk</h1>
@@ -7,10 +42,8 @@ export default function AddProduct() {
           <span className="label-text">Kategori Produk</span>
         </div>
         <select className="select select-bordered w-full max-w-xs">
-          <option disabled selected>
-            Pilih
-          </option>
-          <option>Sayuran</option>
+          <option disabled>Pilih</option>
+          <option>Choose product</option>
           <option>Buah</option>
         </select>
       </label>
@@ -21,102 +54,25 @@ export default function AddProduct() {
         </div>
 
         <ul className="grid gap-4 grid-cols-2 lg:grid-cols-6 md:grid-cols-4 w-full">
-          <li>
-            <input
-              type="radio"
-              id="bawang-merah"
-              name="hosting"
-              defaultValue="hosting-small"
-              className="hidden peer"
-              required=""
-            />
-            <label
-              htmlFor="bawang-merah"
-              className="inline-flex items-center justify-between w-full p-2 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer peer-checked:border-blue-primary peer-checked:text-blue-primary hover:text-gray-600 hover:bg-gray-100">
-              <div className="block">
-                <div className="mask rounded">
-                  <img
-                    className="mask rounded aspect-square"
-                    src="https://images.unsplash.com/photo-1565685225009-fc85d9109c80"
-                    alt="Bawang Merah Image"
-                  />
-                </div>
-                <div className="w-full text-sm mt-3 mb-2 ">Bawang Merah</div>
-              </div>
-            </label>
-          </li>
-          <li>
-            <input
-              type="radio"
-              id="cabe-rawit"
-              name="hosting"
-              defaultValue="hosting-small"
-              className="hidden peer"
-              required=""
-            />
-            <label
-              htmlFor="cabe-rawit"
-              className="inline-flex items-center justify-between w-full p-2 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer peer-checked:border-blue-primary peer-checked:text-blue-primary hover:text-gray-600 hover:bg-gray-100">
-              <div className="block">
-                <img
-                  className="mask rounded aspect-square"
-                  src="https://images.unsplash.com/photo-1546860255-95536c19724e"
-                  alt="Cabe Rawit Merah Image"
-                />
-                <div className="w-full text-sm mt-3 mb-2 ">
-                  Cabe Rawit Merah
-                </div>
-              </div>
-            </label>
-          </li>
-          <li>
-            <input
-              type="radio"
-              id="bawang-merah1"
-              name="hosting"
-              defaultValue="hosting-small"
-              className="hidden peer"
-              required=""
-            />
-            <label
-              htmlFor="bawang-merah1"
-              className="inline-flex items-center justify-between w-full p-2 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer peer-checked:border-blue-primary peer-checked:text-blue-primary hover:text-gray-600 hover:bg-gray-100">
-              <div className="block">
-                <div className="mask rounded">
-                  <img
-                    className="mask rounded aspect-square"
-                    src="https://images.unsplash.com/photo-1565685225009-fc85d9109c80"
-                    alt="Bawang Merah Image"
-                  />
-                </div>
-                <div className="w-full text-sm mt-3 mb-2 ">Bawang Merah</div>
-              </div>
-            </label>
-          </li>
-          <li>
-            <input
-              type="radio"
-              id="cabe-rawit1"
-              name="hosting"
-              defaultValue="hosting-small"
-              className="hidden peer"
-              required=""
-            />
-            <label
-              htmlFor="cabe-rawit1"
-              className="inline-flex items-center justify-between w-full p-2 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer peer-checked:border-blue-primary peer-checked:text-blue-primary hover:text-gray-600 hover:bg-gray-100">
-              <div className="block">
-                <img
-                  className="mask rounded aspect-square"
-                  src="https://images.unsplash.com/photo-1546860255-95536c19724e"
-                  alt="Cabe Rawit Merah Image"
-                />
-                <div className="w-full text-sm mt-3 mb-2 ">
-                  Cabe Rawit Merah
-                </div>
-              </div>
-            </label>
-          </li>
+          {products &&
+            products.map((el, index) => {
+              return (
+                <li key={index} onClick={() => setSelectedProduct(el)}>
+                  <input type="radio" id={el.productName} name="hosting" defaultValue="hosting-small" className="hidden peer" required="" />
+                  <label
+                    htmlFor={el.productName}
+                    className="inline-flex items-center justify-between w-full p-2 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer peer-checked:border-blue-primary peer-checked:text-blue-primary hover:text-gray-600 hover:bg-gray-100"
+                  >
+                    <div className="block">
+                      <div className="mask rounded">
+                        <img className="mask rounded aspect-square" src="https://images.unsplash.com/photo-1565685225009-fc85d9109c80" alt="Bawang Merah Image" />
+                      </div>
+                      <div className="w-full text-sm mt-3 mb-2 ">{el.productName}</div>
+                    </div>
+                  </label>
+                </li>
+              );
+            })}
         </ul>
       </label>
 
@@ -124,27 +80,17 @@ export default function AddProduct() {
         <div className="label">
           <span className="label-text">Jumlah Stok</span>
         </div>
-        <input
-          type="number"
-          placeholder="Type here"
-          min="0"
-          className="input input-bordered w-full max-w-xs"
-        />
+        <input type="number" placeholder="Type here" min="0" className="input input-bordered w-full max-w-xs" />
       </label>
 
       <label className="form-control w-full max-w-xs mb-2">
         <div className="label">
           <span className="label-text">Harga</span>
         </div>
-        <input
-          type="number"
-          placeholder="Type here"
-          min="0"
-          className="input input-bordered w-full max-w-xs"
-        />
+        <input type="number" placeholder="Type here" min="0" className="input input-bordered w-full max-w-xs" />
         <div className="label">
           <span className="label-text-alt">
-            Kisaran harga: Rp34,500.00 - Rp37,000.00 / kg
+            Kisaran harga: {selectedProduct && selectedProduct.HER} - {selectedProduct && selectedProduct.HET} / {selectedProduct && selectedProduct.unit}
           </span>
         </div>
       </label>
