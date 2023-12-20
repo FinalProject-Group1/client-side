eff750883b3200a5f8353fe3f2c2fa2ccd6041e1
Kevin Fami Anggara <kevinfanggara@gmail.com>
2023-12-20 22:44:21 +0700

    revamp seller profile UI

--- src/components/seller-components/SellerEditAccount.jsx
+++ src/components/seller-components/SellerEditAccount.jsx
@@ -1,68 +1,147 @@
 export default function SellerEditAccount() {
   return (
     <section id="seller-account" className="p-8">
-      <h1 className="text-xl font-bold mb-2">Akun Pengguna</h1>
-      <label className="form-control w-full max-w-xs mb-2">
-        <div className="label">
-          <span className="label-text">Nama Toko</span>
+      <div className="w-2/3 flex-col">
+        <div className="grid lg:grid-cols-2 md:grid-cols-2 gap-4">
+          <div className="card bg-base-100 shadow">
+            <div className="card-body">
+              <div className="card-title text-base">Nama Lengkap:</div>
+              <span>Jokowi Dodo</span>
+              <div className="absolute top-5 right-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  data-slot="icon"
+                  className="w-8 h-8 stroke-green-primary">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className="card bg-base-100 shadow">
+            <div className="card-body">
+              <div className="card-title text-base">Nama Toko:</div>
+              <span>Toko Rajawali Merah</span>
+              <div className="absolute top-5 right-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  data-slot="icon"
+                  className="w-8 h-8 stroke-green-primary">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className="card bg-base-100 shadow">
+            <div className="card-body">
+              <div className="card-title text-base">Nomor Handphone:</div>
+              <span>09811237485</span>
+              <div className="absolute top-5 right-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  data-slot="icon"
+                  className="w-8 h-8 stroke-green-primary">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className="card bg-base-100 shadow">
+            <div className="card-body">
+              <div className="card-title text-base">Alamat:</div>
+              <span>Jalan Sudirman Raya No. 21A</span>
+              <div className="absolute top-5 right-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  data-slot="icon"
+                  className="w-8 h-8 stroke-green-primary">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className="card bg-base-100 shadow">
+            <div className="card-body">
+              <div className="card-title text-base">Kota/Kabupaten:</div>
+              <span>Jakarta Timur</span>
+              <div className="absolute top-5 right-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  data-slot="icon"
+                  className="w-8 h-8 stroke-green-primary">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                  />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className="card bg-base-100 shadow">
+            <div className="card-body">
+              <div className="card-title text-base">Saldo:</div>
+              <span>Rp1.000.000.000,00</span>
+              <div className="absolute top-5 right-5">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  data-slot="icon"
+                  className="w-8 h-8 stroke-green-primary">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
         </div>
-        <input
-          type="text"
-          placeholder="Type here"
-          className="input input-bordered w-full max-w-xs"
-        />
-      </label>
-      <label className="form-control w-full max-w-xs mb-2">
-        <div className="label">
-          <span className="label-text">Nama Lengkap</span>
-        </div>
-        <input
-          type="text"
-          placeholder="Type here"
-          className="input input-bordered w-full max-w-xs"
-        />
-      </label>
-      <label className="form-control w-full max-w-xs mb-2">
-        <div className="label">
-          <span className="label-text">Email</span>
-        </div>
-        <input
-          type="email"
-          placeholder="Type here"
-          className="input input-bordered w-full max-w-xs"
-        />
-      </label>
-      <label className="form-control w-full max-w-xs mb-2">
-        <div className="label">
-          <span className="label-text">Nomor Handphone</span>
-        </div>
-        <input
-          type="text"
-          placeholder="Type here"
-          className="input input-bordered w-full max-w-xs"
-        />
-      </label>
-      <label className="form-control w-full max-w-xs mb-2">
-        <div className="label">
-          <span className="label-text">Kota/Kabupaten</span>
-        </div>
-        <select className="select select-bordered w-full max-w-xs">
-          <option selected>Jakarta Timur</option>
-          <option>Jakarta Selatan</option>
-          <option>Jakarta Pusat</option>
-          <option>Jakarta Barat</option>
-          <option>Jakarta Utara</option>
-        </select>
-      </label>
-      {/* <label className="form-control w-full max-w-xs mb-2">
-        <div className="label">
-          <span className="label-text">Alamat</span>
-        </div>
-        <textarea
-          className="textarea textarea-bordered h-28"
-          placeholder="Alamat lengkap"></textarea>
-      </label> */}
-      <button className="btn bg-blue-primary text-white my-3">Simpan</button>
+      </div>
     </section>
   );
 }
