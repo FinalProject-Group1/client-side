e703d29919b0502b28ac3bd02f120fb530112ef9
benisprlh <benisaprulah5@gmail.com>
2023-12-20 20:04:27 +0700

    wiring profile done

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -16,104 +16,104 @@ import Login from '../pages/Login';
 import { getRole, getToken } from '../features/user/actions';
 
 const router = createBrowserRouter([
-	{
-		path: '/',
-		loader: () => {
-			if (getToken() && getRole() === 'seller') {
-				return redirect('/seller');
-			}
-			return null;
-		},
-		children: [
-			{
-				path: '',
-				element: <Home />,
-			},
-			{
-				path: '/products/:productId',
-				element: <ListSellerProducts />,
-			},
-			{
-				path: 'shop/:sellerId',
-				element: <ShopSeller />,
-			},
-			{
-				path: 'checkout/:invoiceId',
-				loader: () => {
-					return getToken() ? null : redirect('/login');
-				},
-				element: <Shipment />,
-			},
-			{
-				path: 'transaction',
-				loader: () => {
-					return getToken() ? null : redirect('/login');
-				},
-				element: <OrdersList />,
-			},
-		],
-	},
-	{
-		path: '/',
-		loader: () => {
-			if (getToken()) {
-				if (getRole() === 'buyer') return redirect('/');
-				if (getRole() === 'seller') return redirect('/seller');
-			}
-			return null;
-		},
-		children: [
-			{
-				path: 'register',
-				element: <Register />,
-			},
-			{
-				path: 'login',
-				element: <Login />,
-			},
-		],
-	},
-	{
-		path: '/seller',
-		loader: () => {
-			if (!getToken()) return redirect('/login');
-			if (getRole() === 'buyer' && getToken()) {
-				return redirect('/');
-			}
-			return null;
-		},
-		element: <Dashboard />,
-		children: [
-			{
-				path: '',
-				loader: () => redirect('/seller/transaction'),
-			},
-			{
-				path: 'transaction',
-				element: <OrdersList />,
-			},
-			{
-				path: 'transaction/:id',
-				element: <OrderDetails />,
-			},
-			{
-				path: 'add-product',
-				element: <AddProduct />,
-			},
-			{
-				path: 'products',
-				element: <ProductsList />,
-			},
-			{
-				path: 'products/:id',
-				element: <EditProduct />,
-			},
-			{
-				path: 'account',
-				element: <SellerEditAccount />,
-			},
-		],
-	},
+  {
+    path: '/',
+    loader: () => {
+      if (getToken() && getRole() === 'seller') {
+        return redirect('/seller');
+      }
+      return null;
+    },
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: '/products/:productId',
+        element: <ListSellerProducts />,
+      },
+      {
+        path: 'shop/:sellerId',
+        element: <ShopSeller />,
+      },
+      {
+        path: 'checkout/:invoiceId',
+        loader: () => {
+          return getToken() ? null : redirect('/login');
+        },
+        element: <Shipment />,
+      },
+      {
+        path: 'transaction',
+        loader: () => {
+          return getToken() ? null : redirect('/login');
+        },
+        element: <OrdersList />,
+      },
+    ],
+  },
+  {
+    path: '/',
+    loader: () => {
+      if (getToken()) {
+        if (getRole() === 'buyer') return redirect('/');
+        if (getRole() === 'seller') return redirect('/seller');
+      }
+      return null;
+    },
+    children: [
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+    ],
+  },
+  {
+    path: '/seller',
+    loader: () => {
+      if (!getToken()) return redirect('/login');
+      if (getRole() === 'buyer' && getToken()) {
+        return redirect('/');
+      }
+      return null;
+    },
+    element: <Dashboard />,
+    children: [
+      {
+        path: '',
+        loader: () => redirect('/seller/transaction'),
+      },
+      {
+        path: 'transaction',
+        element: <OrdersList />,
+      },
+      {
+        path: 'transaction/:id',
+        element: <OrderDetails />,
+      },
+      {
+        path: 'add-product',
+        element: <AddProduct />,
+      },
+      {
+        path: 'products',
+        element: <ProductsList />,
+      },
+      {
+        path: 'products/:id',
+        element: <EditProduct />,
+      },
+      {
+        path: 'account',
+        element: <SellerEditAccount />,
+      },
+    ],
+  },
 ]);
 
 export default router;
